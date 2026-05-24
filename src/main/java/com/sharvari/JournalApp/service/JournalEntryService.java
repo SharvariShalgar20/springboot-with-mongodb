695421c4adf0d72f4aed64d6d1139545291bf0d3
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-05-24 19:04:08 +0530

    feat : @Transaction annotation added

--- src/main/java/com/sharvari/JournalApp/service/JournalEntryService.java
+++ src/main/java/com/sharvari/JournalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import com.sharvari.JournalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +21,7 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String username)
     {
         Users user = userService.findByUsername(username);
