2ca95360318a54c65129942bb907a82486f1eba8
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-05-28 18:24:42 +0530

    logging added to AdminController

--- src/main/java/com/sharvari/JournalApp/controller/AdminController.java
+++ src/main/java/com/sharvari/JournalApp/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.sharvari.JournalApp.controller;
 
 import com.sharvari.JournalApp.model.Users;
 import com.sharvari.JournalApp.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
     @Autowired
     private UserService userService;
 
@@ -21,13 +25,16 @@ public class AdminController {
         List<Users> users = userService.getAll();
 
         if(users != null && !users.isEmpty()) {
+            logger.info("Returning {} users to admin", users.size());
             return new ResponseEntity<>(users,HttpStatus.OK);
         }
+        logger.warn("No users found in the system");
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/create-admin")
     public void createAdmin(@RequestBody Users user){
         userService.saveAdmin(user);
+        logger.info("Admin user created: {}", user.getUsername());
     }
 }
