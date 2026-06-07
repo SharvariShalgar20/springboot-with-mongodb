7327d0c9cbce538e1352c2978b4e0cffb60131e0
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-06-07 20:18:33 +0530

    feat : npm package installed

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+      port: 3000,
+      proxy: {
+        '/journal': { target: 'http://localhost:8080', changeOrigin: true },
+        '/user':    { target: 'http://localhost:8080', changeOrigin: true },
+        '/public':  { target: 'http://localhost:8080', changeOrigin: true },
+        '/admin':   { target: 'http://localhost:8080', changeOrigin: true },
+      }
+    }
+})
